b5f10f09e7352ee285185ffd8f715947d4b7a1f8
Sebastian Ramirez <desarrollador8@azsmart.net>
2020-07-29 18:06:51 -0500

    Updates

---
+++ precache-manifest.f27ad733dd541c5e6f40ee188b23febd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7afd278d3944a2e86aba53ba8c6f373",
+    "url": "/Pacientes.git/index.html"
+  },
+  {
+    "revision": "2fa5d794af44b7b49034",
+    "url": "/Pacientes.git/static/css/main.4c798faf.chunk.css"
+  },
+  {
+    "revision": "8315ab8a2143c7e5fffe",
+    "url": "/Pacientes.git/static/js/2.7aab9922.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pacientes.git/static/js/2.7aab9922.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fa5d794af44b7b49034",
+    "url": "/Pacientes.git/static/js/main.04ec0123.chunk.js"
+  },
+  {
+    "revision": "24960f67081c9969f10a",
+    "url": "/Pacientes.git/static/js/runtime-main.cffbe393.js"
+  }
+]);
