496d13886ba678fda8572665a654b9bc5e96dd09
Sebastian Ramirez <desarrollador8@azsmart.net>
2020-07-29 18:01:40 -0500

    Updates

---
+++ precache-manifest.c69b157f95e031b9e4d1d50ad1f57f53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82cb83bcf38f96f7a3b90f2da2cf9c63",
+    "url": "/19sebastian95/Pacientes.git/index.html"
+  },
+  {
+    "revision": "756af2980a35672b8c1f",
+    "url": "/19sebastian95/Pacientes.git/static/css/main.4c798faf.chunk.css"
+  },
+  {
+    "revision": "8315ab8a2143c7e5fffe",
+    "url": "/19sebastian95/Pacientes.git/static/js/2.7aab9922.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/19sebastian95/Pacientes.git/static/js/2.7aab9922.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "756af2980a35672b8c1f",
+    "url": "/19sebastian95/Pacientes.git/static/js/main.17d8e5a2.chunk.js"
+  },
+  {
+    "revision": "05108a3eb4dfc39c30bb",
+    "url": "/19sebastian95/Pacientes.git/static/js/runtime-main.cc952d27.js"
+  }
+]);
